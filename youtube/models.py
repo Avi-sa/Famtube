e85aff94301e2b239922a147427e87bb02b9faeb
Avi-sa <avivish000@gmail.com>
2022-06-18 04:45:14 +0530

    Video fetch Function Created and model for apikey

--- youtube/models.py
+++ youtube/models.py
@@ -5,10 +5,11 @@ from django.db import models
 class YoutubeVideo(models.Model):
     video_id = models.CharField(max_length=30, db_index=True, primary_key=True)
     channel_id = models.CharField(max_length=30)
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=300)
+    title = models.CharField(max_length=70)
+    description = models.TextField()
+    thumbnail_urls = models.JSONField()
     duration = models.IntegerField(default=0, null=True, blank=True)
-    channel_name = models.CharField(max_length=30)
+    channel_title = models.CharField(max_length=30)
     published_at = models.DateTimeField(default=datetime.now())
     modified_at = models.DateTimeField(auto_now=True)
 
@@ -17,3 +18,15 @@ class YoutubeVideo(models.Model):
 
     def __str__(self):
         return self.video_id + " " + self.title
+
+class APIkey(models.Model):
+    api_key = models.CharField(max_length=100)
+    is_expired = models.BooleanField(default=False)
+    added_on = models.DateTimeField(auto_now_add=True)
+    expired_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = "youtube"
+
+    def __str__(self):
+        return self.api_key + " " + self.is_expired
--- youtube/scheduled_task.py
+++ youtube/scheduled_task.py
@@ -1,5 +1,52 @@
+from sre_constants import SUCCESS
 from celery import shared_task
+from googleapiclient.discovery import build
+from isodate import parse_duration
+from youtube.models import YoutubeVideo
 
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+PREDEFINED_QUERY = "Cricket"
+
+API_KEY = "AIzaSyBkIua0rxsm_3eCD-CM5JKKDsq8el86rPg"
+
+
+def google_api():
+    famtube = build(YOUTUBE_API_SERVICE_NAME, 
+                    YOUTUBE_API_VERSION, developerKey=API_KEY)
+    search_result = famtube.search().list(
+                        q = PREDEFINED_QUERY, 
+                        part = 'snippet', maxResults=5, type='video',
+                        order='date', publishedAfter='2021-01-01T00:00:00Z'
+                    ).execute()
+    videos_ids = []
+    items_list = search_result.get('items', [])
+    for item in items_list:
+        videos_ids.append(item['id']['videoId'])
+    videos_list = famtube.videos().list(id=videos_ids, part='snippet,contentDetails').execute()
+    items_list = videos_list.get('items', [])
+    for item in items_list:
+        video_id = item['id']
+        published_at = item['snippet']['publishedAt']
+        channel_id = item['snippet']['channelId']
+        title = item['snippet']['title']
+        description = item['snippet']['description']
+        channel_title = item['snippet']['channelTitle']
+        thumbnails = item['snippet']['thumbnails']
+        duration = int(parse_duration(item['contentDetails']['duration']).total_seconds() / 60)
+        # youtube = YoutubeVideo(
+        #     video_id = video_id,
+        #     channel_id = channel_id,
+        #     title = title,
+        #     description = description,
+        #     channel_name = channel_title,
+        #     thumbnail_urls = thumbnails,
+        #     published_at = published_at,
+        #     duration = duration
+        # )
+        print('SUCCESS', ' ', video_id, channel_id, published_at, title, description, channel_title, duration)
+        print('\n\n')
+        # youtube.save()
 
 @shared_task(bind=True)
 def celery_test(self):
